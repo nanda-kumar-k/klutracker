bd0992087cbf5065e437de9f8e0344b4ffcfb08b
N-and-U <nandakumar.k05@gmail.com>
2022-08-19 11:13:28 +0530

    models updated

--- klutracker/settings.py
+++ klutracker/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'klutrackerapi',
+    # 'rest_framework',
 ]
 
 MIDDLEWARE = [
@@ -75,8 +77,15 @@ WSGI_APPLICATION = 'klutracker.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'klutracker',
+        'USER': 'root',
+        'PASSWORD': 'mVre9392',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+        }
     }
 }
 
